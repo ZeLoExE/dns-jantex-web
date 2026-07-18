13d668a9ef6f103f432d666e8c1ced8c95d435f2
JanTex <aidinkameli1@gmail.com>
2026-07-18 17:30:45 +0330

    chore: scaffold Vue 3 + Vite project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app" :class="{ dark: isDark }">
+    <h1>DNS Jantex Web</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useThemeStore } from './stores/theme'
+
+const themeStore = useThemeStore()
+const isDark = computed(() => themeStore.isDark)
+</script>
